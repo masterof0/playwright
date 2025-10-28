dc7d7ed53dbbfec6fef2e20b73ce9733c33b5e4d
chjohnson <chjohnson@vectra.ai>
2025-10-28 17:17:01 -0500

    add print statements to show stages

--- conftest.py
+++ conftest.py
@@ -4,18 +4,25 @@ from playwright.sync_api import Playwright, Browser, expect, sync_playwright
 
 
 def pytest_configure(config):
-    print(config)
+    # print(dir(config))
+    print("configure")
 
 
 def pytest_sessionstart(session):
-    print(session)
+    # print(dir(session))
+    print("sessionstart")
+
+
+def pytest_sessionfinish(session):
+    # print(dir(session))
+    print("sessionfinish")
 
 
 @pytest.fixture(scope="session", autouse=True)
 def setup(request):
-    print("setup test")
+    print("fixture")
     request.addfinalizer(cleanup)
 
 
 def cleanup():
-    print("completed all tests")
+    print("cleanup")
